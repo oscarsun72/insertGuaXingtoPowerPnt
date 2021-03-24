1ef73183ad6026620351d2ebd64e70ef80a2b826
守真 孫 <oscarsun72@hotmail.com>
2021-03-24 22:32:12 +0800

    OK

--- insertGuaXingtoPowerpnt/insertGuaXingtoPowerpnt/Form1.Designer.cs
+++ insertGuaXingtoPowerpnt/insertGuaXingtoPowerpnt/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.textBox1.Size = new System.Drawing.Size(640, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "fileFullName";
+            this.textBox1.Visible = false;
             // 
             // button1
             // 
@@ -48,16 +49,17 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(657, 304);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "簡報插入64卦圖";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.ResumeLayout(false);
--- insertGuaXingtoPowerpnt/insertGuaXingtoPowerpnt/Form1.cs
+++ insertGuaXingtoPowerpnt/insertGuaXingtoPowerpnt/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.Office.Interop.PowerPoint;
+using PowerPnt = Microsoft.Office.Interop.PowerPoint;
 
 namespace insertGuaXingtoPowerpnt
 {
@@ -25,15 +25,70 @@ namespace insertGuaXingtoPowerpnt
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
+            string dir = getDir() + "\\";
+            if (dir != "")
+            {
+                PowerPnt.Presentation ppt = getPPT();
+                PowerPnt.Selection sel = ppt.Application.ActiveWindow.Selection;
+                if (sel.Type == PowerPnt.PpSelectionType.ppSelectionText)
+                {
+                    PowerPnt.Slide sld = ppt.Application.ActiveWindow.View.Slide;
+                    string f = dir + sel.TextRange.Text + ".png";
+                    if (System.IO.File.Exists(f))
+                    {
+                        PowerPnt.TextRange tr= sel.TextRange.InsertAfter(sel.TextRange.Text);
+                        tr.Select();
+                        sel = sel.Application.ActiveWindow.Selection;
+                        float lf = sel.TextRange.BoundLeft;
+                        float tp = sel.TextRange.BoundTop;
+                        int selCt = sel.TextRange2.Characters.Count;
+                        float h = sel.TextRange.BoundHeight;
+                        if (sel.ShapeRange.TextFrame.Orientation==Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationVerticalFarEast)
+                        {
+                            h = sel.TextRange.BoundHeight / selCt;
+                        }
+                        float w = sel.TextRange.BoundWidth;                        
+                        if (sel.ShapeRange.TextFrame.Orientation==Microsoft.Office.Core.MsoTextOrientation.msoTextOrientationHorizontal)
+                        {
+                            w= sel.TextRange.BoundWidth/selCt  ;
+                        }
+                        PowerPnt.Shape spr = sld.Shapes.AddPicture(f,Microsoft.Office.Core.MsoTriState.msoFalse
+                            ,Microsoft.Office.Core.MsoTriState.msoTrue,
+                            lf,tp,w,h);
+                        sel.TextRange.Text = "　";
+                        
+                    }
 
+                }
+                ppt.Application.Activate();
+            }
         }
 
-        Presentation getPPT()
+        PowerPnt.Presentation getPPT()
         {
-            Presentations ppts =PresentationClass;
-            Presentation ppt= ppts.Open(textBox1.Text);
+            //https://docs.microsoft.com/en-us/previous-versions/office/troubleshoot/office-developer/use-visual-c-automate-run-program-instance
+            PowerPnt.Application oPPntApp =
+                (PowerPnt.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("PowerPoint.Application");
+            //機碼：HKEY_CLASSES_ROOT\PowerPoint.Application
+            PowerPnt.Presentation ppt = oPPntApp.ActivePresentation;
 
-            return;
+            return ppt;
+        }
+
+        string getDir()
+        {
+            string dir = "";
+            List<string> dirs = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            foreach (string item in dirs)
+            {
+                dir = item + ":\\@@@華語文工具及資料@@@\\Macros\\64卦圖";
+                if (System.IO.Directory.Exists(dir))
+                {
+
+                    return dir;
+                }
+            }
+            return "";
         }
     }
 }
