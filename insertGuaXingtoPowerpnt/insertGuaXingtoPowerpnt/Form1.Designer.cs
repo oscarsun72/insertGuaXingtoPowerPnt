9133b473326d120b08fc1a53e735d5cb5a4bca0f
守真 孫 <oscarsun72@hotmail.com>
2021-03-26 16:58:53 +0800

    小篆、隸書大致OK

--- insertGuaXingtoPowerpnt/insertGuaXingtoPowerpnt/Form1.Designer.cs
+++ insertGuaXingtoPowerpnt/insertGuaXingtoPowerpnt/Form1.Designer.cs
@@ -34,23 +34,26 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 185);
+            this.textBox1.Location = new System.Drawing.Point(18, 278);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(640, 22);
+            this.textBox1.Size = new System.Drawing.Size(958, 29);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "fileFullName";
             this.textBox1.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(208, 137);
+            this.button1.Location = new System.Drawing.Point(312, 206);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(79, 32);
+            this.button1.Size = new System.Drawing.Size(118, 48);
             this.button1.TabIndex = 1;
             this.button1.Text = "Go~";
             this.button1.UseVisualStyleBackColor = true;
@@ -60,20 +63,22 @@
             // 
             this.listBox1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 21;
-            this.listBox1.Location = new System.Drawing.Point(11, 18);
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(16, 27);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(111, 151);
+            this.listBox1.Size = new System.Drawing.Size(164, 196);
             this.listBox1.TabIndex = 2;
             // 
             // listBox2
             // 
             this.listBox2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 21;
-            this.listBox2.Location = new System.Drawing.Point(176, 18);
+            this.listBox2.ItemHeight = 32;
+            this.listBox2.Location = new System.Drawing.Point(264, 27);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(111, 67);
+            this.listBox2.Size = new System.Drawing.Size(164, 68);
             this.listBox2.TabIndex = 3;
             // 
             // numericUpDown1
@@ -84,17 +89,29 @@
             0,
             0,
             65536});
-            this.numericUpDown1.Location = new System.Drawing.Point(297, 102);
+            this.numericUpDown1.Location = new System.Drawing.Point(446, 153);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(53, 22);
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 29);
             this.numericUpDown1.TabIndex = 4;
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(264, 116);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(157, 22);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Word.InlineShape";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 213);
+            this.ClientSize = new System.Drawing.Size(572, 320);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
@@ -102,6 +119,7 @@
             this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "簡報插入字圖";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -120,6 +138,7 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
