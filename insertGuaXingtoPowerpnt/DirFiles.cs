5c34f7f100425fb392692b661a95bb316e378921
守真 孫 <oscarsun72@hotmail.com>
2021-05-13 20:04:10 +0800

    解壓縮zip時先判斷是否要覆蓋舊有檔案

--- insertGuaXingtoPowerpnt/DirFiles.cs
+++ insertGuaXingtoPowerpnt/DirFiles.cs
@@ -11,9 +11,10 @@ using powerPnt = Microsoft.Office.Interop.PowerPoint;
 namespace CharacterConverttoCharacterPics
 {
     public class DirFiles
-    {//以後目錄、路徑均要取得最後的反斜線
+    { //以後目錄、路徑均要取得最後的反斜線
 
-        DirectoryInfo dir; internal string TopFolder;
+        DirectoryInfo dir;
+        internal string TopFolder;
         IEnumerable<FileInfo> fileList;
         public DirFiles() { }
         public DirFiles(string topFolder)
@@ -34,26 +35,25 @@ namespace CharacterConverttoCharacterPics
             get
             {
                 return
-                    from f in fileList
-                    where f.Extension.Equals(".png",//原來 Extension 屬性值包括前綴「.」號20210506
+                from f in fileList
+                where f.Extension.Equals(".png", //原來 Extension 屬性值包括前綴「.」號20210506
                     System.StringComparison.OrdinalIgnoreCase)
-                    select f;
+                select f;
             }
         }
 
         internal static string getDirRoot
-        {//https://www.google.com/search?q=c%23+%E5%8F%96%E5%BE%97%E5%B0%88%E6%A1%88%E8%B7%AF%E5%BE%91&rlz=1C1JRYI_enTW948TW948&oq=%E5%8F%96%E5%BE%97%E5%B0%88%E6%A1%88%E8%B7%AF%E5%BE%91&aqs=chrome.1.69i57j0i5i30l2.7266j0j7&sourceid=chrome&ie=UTF-8
+        { //https://www.google.com/search?q=c%23+%E5%8F%96%E5%BE%97%E5%B0%88%E6%A1%88%E8%B7%AF%E5%BE%91&rlz=1C1JRYI_enTW948TW948&oq=%E5%8F%96%E5%BE%97%E5%B0%88%E6%A1%88%E8%B7%AF%E5%BE%91&aqs=chrome.1.69i57j0i5i30l2.7266j0j7&sourceid=chrome&ie=UTF-8
             get =>
                 new DirectoryInfo(
-                System.AppDomain.CurrentDomain.BaseDirectory)
+                    System.AppDomain.CurrentDomain.BaseDirectory)
                 .Parent.Parent.Parent.FullName + "\\";
         }
 
         internal static FileInfo getCjk_basic_IDS_UCS_Basic_txt()
         {
             DirectoryInfo dirRoot = new DirectoryInfo(getDirRoot);
-            IEnumerable<FileInfo> fileList = dirRoot.GetFiles
-                ("*.txt", SearchOption.AllDirectories);
+            IEnumerable<FileInfo> fileList = dirRoot.GetFiles("*.txt", SearchOption.AllDirectories);
             IEnumerable<FileInfo> fileQuery =
                 from file in fileList
                 where file.Name.IndexOf("cjk-basic-IDS-UCS-Basic.txt") > -1
@@ -71,7 +71,7 @@ namespace CharacterConverttoCharacterPics
             if (File.Exists(f))
                 return new FileInfo(@"G:\我的雲端硬碟\programming程式設計開發\fontOkList.txt");
             else
-            {//判斷成功了
+            { //判斷成功了
                 DirectoryInfo dirRoot = new DirectoryInfo(getDirRoot);
                 DirectorySecurity directorySecurity = dirRoot.GetAccessControl();
                 if (directorySecurity.AreAccessRulesProtected)
@@ -82,12 +82,11 @@ namespace CharacterConverttoCharacterPics
                     {
                         MessageBox.Show("此資料夾是無法讀取的！\n\r" +
                             "而「" + f + "」檔案又不存在，故無法執行！", "",
-                        MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        Application.OpenForms[0].Close();//return null;
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.OpenForms[0].Close(); //return null;
                     }
                 }
-                IEnumerable<FileInfo> fileList = dirRoot.GetFiles
-                    ("*.txt", SearchOption.AllDirectories);
+                IEnumerable<FileInfo> fileList = dirRoot.GetFiles("*.txt", SearchOption.AllDirectories);
                 IEnumerable<FileInfo> fileQuery =
                     from file in fileList
                     where file.Name.IndexOf("fontOkList.txt") > -1
@@ -125,7 +124,7 @@ namespace CharacterConverttoCharacterPics
         }
 
         //"Macros\\古文字\\"
-        internal static string PicsRootFolder//"Macros\\古文字\\"
+        internal static string PicsRootFolder //"Macros\\古文字\\"
         { get => Path華語文工具及資料 + "Macros\\古文字\\"; }
 
         internal static string Path華語文工具及資料
@@ -135,7 +134,7 @@ namespace CharacterConverttoCharacterPics
                 string dir;
                 DriveInfo[] dis = DriveInfo.GetDrives();
                 //List<string> dirs = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-                foreach (DriveInfo di in dis)//(string item in dirs)
+                foreach (DriveInfo di in dis) //(string item in dirs)
                 {
                     //dir = item + ":\\@@@華語文工具及資料@@@" + subFolder;
                     dir = di.Name + "@@@華語文工具及資料@@@\\";
@@ -148,7 +147,7 @@ namespace CharacterConverttoCharacterPics
             }
         }
         //without \
-        internal static string getPicDir(picEnum pE)//without \
+        internal static string getPicDir(picEnum pE) //without \
         {
             string subFolder;
             switch (pE)
@@ -162,25 +161,27 @@ namespace CharacterConverttoCharacterPics
                 case picEnum.小篆:
                     subFolder = "Macros\\古文字\\台大說文小篆字圖";
                     break;
-                default://路徑特殊的就析出寫在上面20210410
+                default: //路徑特殊的就析出寫在上面20210410
                     subFolder = "Macros\\古文字\\" +
-                       Application.OpenForms[0].Controls["listBox1"].Text;
+                        Application.OpenForms[0].Controls["listBox1"].Text;
                     break;
             }
             return Path華語文工具及資料 + subFolder;
         }
         internal static string getFullNameNTUswxz(string dir, string x)
-        {//為免ADO存取資料庫失敗而增此
+        { //為免ADO存取資料庫失敗而增此
             return new FindFileThruLINQ().getfilefullnameIn古文字(x, dir);
         }
 
         public static bool IsReadonly(DirectoryInfo di)
         {
-            if (di.Attributes.ToString().IndexOf(FileAttributes.ReadOnly.ToString()) != -1) return true; else return false;
+            if (di.Attributes.ToString().IndexOf(FileAttributes.ReadOnly.ToString()) != -1) return true;
+            else return false;
         }
         public static bool IsReadonly(FileInfo fi)
         {
-            if (fi.Attributes.ToString().IndexOf(FileAttributes.ReadOnly.ToString()) != -1) return true; else return false;
+            if (fi.Attributes.ToString().IndexOf(FileAttributes.ReadOnly.ToString()) != -1) return true;
+            else return false;
         }
         public static void SwitchReadonly(DirectoryInfo di)
         {
@@ -203,7 +204,7 @@ namespace CharacterConverttoCharacterPics
         public static void DeleteFileRemoveReadOnly(FileInfo fileInfo)
         {
             NoReadonly(fileInfo);
-            fileInfo.Delete();//上下兩式作用相同
+            fileInfo.Delete(); //上下兩式作用相同
             //io.File.Delete(fileInfo.FullName);//delete from the source file                            
         }
 
@@ -216,12 +217,12 @@ namespace CharacterConverttoCharacterPics
             if (File.Exists(fZip)) File.Delete(fZip);
             ZipFile.CreateFromDirectory(dir, fZip,
                 CompressionLevel.NoCompression, true
-                );
+            );
         }
 
         internal static void unZipsFromSpecificFolder(DirectoryInfo di)
         {
-            if (!Directory.Exists(di.FullName)) return;
+            if (!Directory.Exists(di.FullName)) return;//di是解壓縮的top folder
             IEnumerable<FileInfo> fis =
                 from file in di.GetFiles("*.zip", SearchOption.TopDirectoryOnly)
                 select file;
@@ -229,7 +230,17 @@ namespace CharacterConverttoCharacterPics
             {
                 try
                 {
-                    ZipFile.ExtractToDirectory(item.FullName, di.FullName);
+                    if (Directory.Exists(di.FullName + "\\" + item.Name.Replace(item.Extension, "")))
+                    {
+                        if (MessageBox.Show("目的資料夾已存在，是否覆寫？", "", MessageBoxButtons.OKCancel,
+                            MessageBoxIcon.Warning) == DialogResult.OK)
+                        {
+                            Directory.Delete(di.FullName + "\\" + item.Name.Replace(item.Extension, ""));
+                            ZipFile.ExtractToDirectory(item.FullName, di.FullName);
+                        }
+                    }
+                    else
+                        ZipFile.ExtractToDirectory(item.FullName, di.FullName);
                     DeleteFileRemoveReadOnly(item);
                 }
                 catch (Exception e) { MessageBox.Show(e.ToString()); }
